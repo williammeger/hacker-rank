c82e3e42b6616f7a8417cb4b12845354e6c872d6
William Meger <wmeger14@gmail.com>
2017-02-16 18:56:10 -0500

    day 1

---
+++ 30-Days-of-Code/01-Data-Types/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+  
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+    
+        Scanner scan = new Scanner(System.in);
+
+        int myInt = scan.nextInt()
+        double myDouble = scan.nextDouble();
+        scan.nextLine();
+        String myString = scan.nextLine();
+    
+        System.out.println(i + myInt);
+        System.out.println(d + myDouble);
+        System.out.println(s + myString);
+
+        scan.close();
+    }
+}
