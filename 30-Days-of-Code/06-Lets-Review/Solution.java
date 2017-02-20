539cd0bff1fb0f11b0dfb44c7985e24e72bdf221
William Meger <wmeger14@gmail.com>
2017-02-20 13:30:18 -0500

    solved day 6

--- 30-Days-of-Code/06-Lets-Review/Solution.java
+++ 30-Days-of-Code/06-Lets-Review/Solution.java
@@ -1,11 +1,29 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-  }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            int stringCount = sc.nextInt();
+            sc.nextLine();
+            for (int i = 0; i <= stringCount; i++) {
+              String s = sc.nextLine();
+              char[] stringAsCharArray = s.toCharArray();
+              for (int j = 0; j < stringAsCharArray.length; j++) {
+                if (j % 2 == 0) {
+                  System.out.print(stringAsCharArray[j]);
+                }
+              }
+              System.out.print(" ");
+              for (int j = 0; j < stringAsCharArray.length; j++) {
+                if (j % 2 != 0) {
+                   System.out.print(stringAsCharArray[j]);
+                }
+              }
+            System.out.println();
+            }    
+        }
+    }
 }
