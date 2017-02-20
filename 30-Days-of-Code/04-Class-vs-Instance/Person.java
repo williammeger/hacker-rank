7f3e45b290e58fa3d449d4c832d5463521ca5f0c
William Meger <wmeger14@gmail.com>
2017-02-20 00:39:02 -0500

    solution for day4

--- 30-Days-of-Code/04-Class-vs-Instance/Person.java
+++ 30-Days-of-Code/04-Class-vs-Instance/Person.java
@@ -1,26 +1,32 @@
 import java.io.*;
 import java.util.*;
 public class Person {
-  private int initialAge;
+
+  private int age;
 
   public Person (int initialAge) {
-    if () {
-      
-    } 
-    this.initialAge = initialAge;
+    if (initialAge < 0) {
+      System.out.println("Age is not valid, setting age to 0.");
+      this.age = 0;
+    } else {
+      this.age = initialAge;
+    }
   }
 
   public void yearPasses() {
-    initialAge += 1;
+    this.age++;
   }
 
   public void amIOld() {
-
+    if(age < 13) {
+      System.out.println("You are young.");
+    } else if (age >= 13 && this.age < 18) {
+      System.out.println("You are a teenager.");
+    } else {
+      System.out.println("You are old.");
+    }
   }
-  // setter for age
-  public int setAge(int age) {
-
-  }
+  
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
