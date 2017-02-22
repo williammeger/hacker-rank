c781277644e81433abe08a507344bf1f98110be5
William Meger <wmeger14@gmail.com>
2017-02-21 22:19:34 -0500

    start day 10

---
+++ 30-Days-of-Code/10-Binary-Numbers/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+
+  }
+  public static int countBinaryOnes(int a) {
+    int counter;
+    int remainder;
+    while (a > 0) {
+
+    }
+  }
+}
