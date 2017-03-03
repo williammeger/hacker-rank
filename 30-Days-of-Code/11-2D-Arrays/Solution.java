dbe8a3810c0afc5cd003ee9493cece552c593eef
William Meger <wmeger14@gmail.com>
2017-03-02 22:57:25 -0500

    start 11

---
+++ 30-Days-of-Code/11-2D-Arrays/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+    public static void main (String[] args) {
+  	Scanner sc = new Scanner(System.in);
+  	int[][] arr = new int[6][6];
+  	for (int i = 0; i < 6; i++) {
+	    for (int j = 0; j < 6; j++) {
+		    arr[i][j] = in.nextInt();
+      }
+    }
+  }
+} 
