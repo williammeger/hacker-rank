51f09554b4f4ec0b20d97bb9b71c9a0b33ef9a91
William Meger <wmeger14@gmail.com>
2017-03-02 22:42:10 -0500

    solved day 10

--- 30-Days-of-Code/10-Binary-Numbers/Solution.java
+++ 30-Days-of-Code/10-Binary-Numbers/Solution.java
@@ -4,13 +4,20 @@ public class Solution {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-
+    int total = countBinaryOnes(n);
+    System.out.println(total);
   }
   public static int countBinaryOnes(int a) {
-    int counter;
-    int remainder;
+    int counter = 0;
+    int max = 0;
     while (a > 0) {
-
-    }
+      if ((a&1) == 1) {
+        counter++;
+        max = Math.max(counter, max);
+      } else {
+        counter = 0;
+      }
+      a = a >> 1;
+    } return max;
   }
 }
