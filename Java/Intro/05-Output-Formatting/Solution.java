833a031d47fcb2c0bc1b385c48ba5de4d160321b
William Meger <wmeger14@gmail.com>
2017-03-28 14:13:52 -0400

    solved 05 formatting

--- Java/Intro/05-Output-Formatting/Solution.java
+++ Java/Intro/05-Output-Formatting/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
        for (int i = 0; i < 3; i++) {
            String s1 = sc.next();
            int x = sc.nextInt();
-           System.out.printf("%1$"); 
+           System.out.printf("%-15s%03d%n", s1, x); 
        }
        System.out.println("================================");
 } 
