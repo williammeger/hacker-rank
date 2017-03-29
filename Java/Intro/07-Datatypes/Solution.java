177e8cb412d6a99439515bd088e621115ddf0542
William Meger <wmeger14@gmail.com>
2017-03-29 18:44:44 -0400

    solved 07 datatypes

--- Java/Intro/07-Datatypes/Solution.java
+++ Java/Intro/07-Datatypes/Solution.java
@@ -3,13 +3,17 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         
-        final double BYTE_LOWER = (int) -(Math.pow(2,7)); //-128
+        final double BYTE_LOWER = -(Math.pow(2,7)); //-128
         final double BYTE_UPPER = Math.pow(2,7) - 1; //127
 
-        final double SHORT_LOWER = (double) -(Math.pow(2,15)); // -32,768
-        final double SHORT_UPPER = (double) (Math.pow(2,15)) - 1; // 32,767
+        final double SHORT_LOWER = -(Math.pow(2,15)); // -32,768
+        final double SHORT_UPPER = Math.pow(2,15) - 1; // 32,767
 
-        final 
+        final double INT_LOWER = -(Math.pow(2,31));
+        final double INT_UPPER = Math.pow(2,31) - 1; 
+
+        final double LONG_LOWER = -(Math.pow(2,63));
+        final double LONG_UPPER = Math.pow(2,63) - 1;
 
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
@@ -19,15 +23,14 @@ public class Solution {
             try {
             long x = sc.nextLong();
             System.out.println(x+ " can be fitted in:");
-            if(x>=-128 && x<=127) System.out.println("* byte");
-            if(x>=-
-
-
+            if(x>=BYTE_LOWER && x<=BYTE_UPPER) System.out.println("* byte");
+            if(x>=SHORT_LOWER && x<=SHORT_UPPER) System.out.println("* short");
+            if(x>=INT_LOWER && x<=INT_UPPER) System.out.println("* int");
+            if(x>=LONG_LOWER && x<=LONG_UPPER) System.out.println("* long");
             }
             catch(Exception e) {
-                System.out.println(sc.nextInt() + " can't be fitted anywhere.");
+                System.out.println(sc.next() + " can't be fitted anywhere.");
             }
-            
         }
     }
 }
