0064bb5ce2b74b60855b37212e05e078c55144b3
William Meger <wmeger14@gmail.com>
2017-04-03 19:50:30 -0400

    solved 03

---
+++ Java/Intro/03-Stdin-and-Stdout-II/Solution.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int i = Integer.parseInt(sc.nextLine());
+        double d = Double.parseDouble(sc.nextLine());
+        String s = sc.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
