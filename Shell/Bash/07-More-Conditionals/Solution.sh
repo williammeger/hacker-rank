d4105b53dcfb89fc4f9d11f8ceb746cf617f3812
William Meger <wmeger14@gmail.com>
2017-04-09 16:35:17 -0400

    solved 07

---
+++ Shell/Bash/07-More-Conditionals/Solution.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+read x
+read y
+read z
+
+if [ $x != $y ] && [ $y != $z ]; then
+    echo "SCALENE"
+elif [ $x == $y ] && [ $y == $z ]; then
+    echo "EQUILATERAL"
+else
+    echo "ISOSCELES"
+fi
