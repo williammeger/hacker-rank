7f7021fe4ba80c680b5f0396c09fd48493b49f30
William Meger <wmeger14@gmail.com>
2017-04-13 00:12:31 -0400

    solved cut#1

---
+++ Java/Strings/04-Anagrams/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String a = sc.next();
+    String b = sc.next();
+
+
+
+
+  }
+  public static boolean isAnagram(String s1, String s2) {
+    if (s1 == null || s2 || null || s1.length() != s2.length) {
+      return false;
+    }
+  }
+}
---
+++ Shell/Text-Processing/00-Cut-#1/Solution.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cut -c3 /dev/stdin
